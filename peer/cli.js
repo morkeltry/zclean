107b464904b6f888059500f966156224037ab2fb
Rémi Bouchar <remi.bouchar@gmail.com>
2019-06-20 07:01:23 +0100

    add basic p2p heartbeat server

--- peer/cli.js
+++ peer/cli.js
@@ -1,12 +1,30 @@
 'use strict'
-
+const _ = require('lodash')
 const asyncRedis = require('async-redis')
+const Buslane = require('buslane')
+const chalk = require('chalk')
 
 const CONFIG = require('./config.json')
-const config = CONFIG[process.argv[2]]
+
+const peerId = parseInt(process.argv[2])
+
+console.log({ peerId, args: process.argv })
+
+const config = CONFIG[peerId]
+
+const { inspect } = require('util')
+
+console.inspect = function (...argv) {
+  console.log(chalk.red('>>> Inspecting'))
+  argv.forEach((arg, i) => {
+    process.stdout.write(`${i}: `)
+    console.log(inspect(arg, { colors: true, depth: null }))
+  })
+  console.log(chalk.red('<<< end'))
+}
 
 if (!config) {
-    throw new Error(`Invalid peer number: ${process.argv[2]}`)
+  throw new Error(`Invalid peer id: ${peerId}`)
 }
 
 console.log('Starting peer with config', { config })
@@ -14,4 +32,51 @@ console.log('Starting peer with config', { config })
 const redis = asyncRedis.createClient(config.redisPort);
 
 
+config.buslane = {
+  name: `peer${peerId}`,
+  shared_api_key: 'Very Secret Indeed',
+  map: []
+}
 
+_.times(CONFIG.length).forEach(key => {
+  config.buslane.map[key] = {
+    name: `peer${key}`,
+    port: 4000 + (key * 1000),
+    ingresses: ['p2p'],
+    // Using default buslane certs for now, very secure :/
+    // ssl_key_path: './ssl/host.key',
+    // ssl_cert_path: './ssl/host.cert'
+  }
+})
+
+console.inspect('CONFIG', config)
+
+const buslane = new Buslane(config.buslane)
+
+const p2p = {
+  ping: () => 'PONG'
+}
+
+buslane.registerIngress('p2p', p2p);
+
+const peers = Object.keys(buslane).filter(s => s.startsWith('peer')).map(peerName => buslane[peerName])
+
+console.inspect(peers)
+
+// p2p loop
+setInterval(async () => {
+  console.log(`peer${peerId}: p2p loop`)
+
+  for (let i = 0; i < peers.length; i++) {
+    try {
+      console.log(`peer ${i} says ${await peers[i].p2p.ping()}`)
+    } catch (err) {
+      console.log(chalk.red(`could not get heartbeat from peer ${i}`))
+    }
+  }
+
+}, 1000)
+
+process.on('uncaughtException', function (err) {
+
+})
--- peer/start.sh
+++ peer/start.sh
@@ -1,1 +1,1 @@
-node cli.js $1
+./node_modules/nodemon/bin/nodemon.js --watch . cli.js --  $1
